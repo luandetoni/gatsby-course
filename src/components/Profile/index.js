15bc260b4f930da42431698f5dfbbc3f2967e3da
Luan <luandetoni@gmail.com>
2022-03-11 08:03:18 -0300

    Modulo 2

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Luan Detoni</h1>
+        <h2>Programador</h2>
+        <p>Membro da empresa Pinnaclo, escrevo sobre o mundo dev.</p>
+    </div>
+)
+
+export default Profile
