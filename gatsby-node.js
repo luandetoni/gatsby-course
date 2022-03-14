774ebdc111e41867a1a941fab7538a6a6299228a
Luan <luandetoni@gmail.com>
2022-03-13 23:54:03 -0300

    Modulo 7

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,18 +37,31 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   return graphql(`
     {
-      allMarkdownRemark {
+      allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
         edges {
           node {
             fields {
               slug
+            }
+            frontmatter {
+              background
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              category
+              description
+              title
+            }
+            timeToRead
+            wordCount {
+              words
             }
           }
         }
       }
     }
   `).then(result => {
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const posts = result.data.allMarkdownRemark.edges
+    
+    posts.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
         component: path.resolve(`./src/templates/blog-post.js`),
@@ -57,6 +70,22 @@ exports.createPages = ({ graphql, actions }) => {
           // in page queries as GraphQL variables.
           slug: node.fields.slug,
         },
+      })
+    })
+
+    const postsPerPage = 5
+    const numPages = Math.ceil(posts.length / postsPerPage)
+
+    Array.from({ length: numPages }).forEach((_, index) => {
+      createPage({
+        path: index === 0 ? `/` : `/page/${index + 1}`,
+        component: path.resolve(`./src/templates/blog-list.js`),
+        context: {
+          limit: postsPerPage,
+          skip: index * postsPerPage,
+          numPages,
+          currentPage: index + 1,
+        }
       })
     })
   })
